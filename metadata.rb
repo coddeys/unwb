2db7adeab4603e09be555eaa5f39ab7d67ead643
Dima Sukhikh <coddeys@gmail.com>
2014-12-22 15:42:16 +0300

    Init berks cookbooks

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'unwb'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures unwb'
+long_description 'Installs/Configures unwb'
+version          '0.1.0'
